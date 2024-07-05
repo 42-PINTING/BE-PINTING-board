d3db1064d473e461f7f29fa74296a771045b1c9d
jeongyunnim <jeongyun7521@gmail.com>
2024-07-05 21:23:51 +0900

    feat: 좋아요 관련 기능 추가 #5
    1. Like entity를 활용한 좋아요 추가/취소/조회 기능 추가

--- board/src/main/java/pinting/board/service/BoardService.java
+++ board/src/main/java/pinting/board/service/BoardService.java
@@ -3,9 +3,11 @@ package pinting.board.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pinting.board.domain.Like;
 import pinting.board.domain.Post;
 import pinting.board.domain.Tag;
 import pinting.board.repository.BoardRepository;
+import pinting.board.repository.LikeRepository;
 import pinting.board.repository.TagRepository;
 
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.Optional;
 public class BoardService {
    private final BoardRepository boardRepository;
    private final TagRepository tagRepository;
+   private final LikeRepository likeRepository;
 
     public Long createPost(Post post) {
       boardRepository.save(post);
@@ -42,16 +45,19 @@ public class BoardService {
        * 업데이트 하려는 태그 리스트를 기존의 태그 리스트로 검색하여 없는 것은 삭제, 있는 것은 패스
        * 비효율적으로 보이기 때문에, 리팩토링을 해야 한다. issue number: #4
        */
+      List<String> targetTags = dto.getTags().stream().distinct().toList();
       List<Tag> originTags = tagRepository.findAllByPostId(postId);
       for (Tag originTag : originTags) {
-         if (!dto.getTags().contains(originTag.name)) {
+         if (!targetTags.contains(originTag.name)) {
             tagRepository.deleteById(originTag.getId());
+            // TODO: originTags에서도 지워주어야 하는지
          }
       }
-      for (String updateTagName : dto.getTags()) {
+      for (String updateTagName : targetTags) {
          if (!originTags.stream().map(Tag::getName).toList().contains(updateTagName)) {
             Tag updateTag = new Tag(updateTagName, findPost.get());
             tagRepository.save(updateTag);
+            // TODO: originTags에도 추가를 해야 하는지
          }
       }
       findPost.get().update(dto);
@@ -75,7 +81,8 @@ public class BoardService {
 
    @Transactional(readOnly = true)
    public List<Post> searchPostByTag(List<String> tags) {
-       return boardRepository.findPostsByTags(tags);
+       List<String> tagsSet = tags.stream().distinct().toList();
+       return boardRepository.findPostsByTags(tagsSet);
    }
 
    @Transactional(readOnly = true)
@@ -84,34 +91,48 @@ public class BoardService {
    }
 
    /**
-    * like 증가
-    * Todo: Like도 엔티티로 두어야 한다.
-    * - 유저가 like한 게시물을 알 수 있어야 함.
+    * like 추가
     */
-   public boolean likePost(Long postId) {
+   public boolean likePost(Long memberId, Long postId) {
       Optional<Post> findPost = boardRepository.findOneById(postId);
 
       if (findPost.isEmpty()) {
          return false;
       }
-      findPost.get().increaseLikeCount();
+      Post targetPost = findPost.get();
+      if (targetPost.getLikes().stream().map(Like::getMemberId).toList().contains(memberId)) {
+         return true;
+      }
+      likeRepository.save(new Like(targetPost, memberId));
       return true;
    }
 
    /**
-    * like 감소
-    * 좋아요 취소만 있어야 한다.
-    * 해당 기능은 좋아요를 누른 사람만 가능해야 하기 때문에 count 증감으로만 처리할 수 없다.
+    * like 취소
     */
-//   public boolean dislikePost(Long postId) {
-//      Optional<Post> findPost = boardRepository.findOneById(postId);
-//
-//      if (findPost.isEmpty()) {
-//         return false;
-//      }
-//      findPost.get().decreaseLikeCount();
-//      return true;
-//   }
+   public boolean cancelLike(Long memberId, Long postId) {
+      Optional<Like> findLike = likeRepository.findOneByMemberIdAndPostId(memberId, postId);
+      if (findLike.isPresent()) {
+         likeRepository.deleteById(findLike.get().getId());
+         return true;
+      }
+      return false;
+   }
+
+   /**
+    * Post like 개수 반환
+    */
+   public int getPostLikesCount(Long postId) {
+      return likeRepository.findAllByPostId(postId).size();
+   }
+
+   /**
+    * Post like 유저 id 목록 반환
+    * Todo: id만 반환하기 때문에, 실제 유저는 Member 서비스에 직접 요청해야 한다.
+    */
+   public List<Long> getPostLikeMembers(Long postId) {
+      return likeRepository.findAllByPostId(postId).stream().map(Like::getMemberId).toList();
+   }
 
    /**
     * 게시물 숨김
